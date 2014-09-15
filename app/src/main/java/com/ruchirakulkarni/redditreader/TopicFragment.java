402d9a48dab6d681f697df8faccabe082b841679
roocheerah <roocheerah@users.noreply.github.com>
2014-09-15 14:10:53 -0700

    Update TopicFragment.java

--- app/src/main/java/com/ruchirakulkarni/redditreader/TopicFragment.java
+++ app/src/main/java/com/ruchirakulkarni/redditreader/TopicFragment.java
@@ -1,36 +1,258 @@
 package com.ruchirakulkarni.redditreader;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class DetailActivity1 extends ActionBarActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by ruchirakulkarni on 9/13/14.
+ */
+public class TopicFragment extends Fragment{
+
+    private ArrayAdapter<String> postTypeAdapter;
+    private String[] posts;
+    String data;
+    private String STRING_URL = "";
+    private final String LOGG_TAG = TopicFragment.class.getSimpleName();
+
+    public TopicFragment() {
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_activity1);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new TopicFragment())
-                    .commit();
-        }
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detail_activity1, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
+        if (id == R.id.action_refresh) {
+            updatePosts();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void updatePosts() {
+        posts = new String[10];
+        FetchTopicTask topicTask = new FetchTopicTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+//        Log.d(LOGG_TAG, "The default topic is " + getString(R.string.pref_topic_default));
+        String topic = prefs.getString(getString(R.string.pref_topic_key), getString(R.string.pref_topic_default));
+//        Log.d(LOGG_TAG, "The topic key is:" + getString(R.string.pref_topic_key));
+//        Log.d(LOGG_TAG, "The String topic is : " + topic);
+        topicTask.execute(topic);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updatePosts();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+         View rootView = inflater.inflate(R.layout.fragment_detail_activity1, container, false);
+
+        //NOW WE NEED TO RECEIVE THE INTENT FROM THE MAINACTIVITY
+
+         Intent intent = getActivity().getIntent();
+         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            data = intent.getStringExtra(Intent.EXTRA_TEXT);
+             data = data.toLowerCase();
+             postTypeAdapter = new ArrayAdapter<String>(
+                     getActivity(), R.layout.list_item_post_textview, R.id.list_item_post_textview, new ArrayList<String>());
+
+             ListView listView = (ListView) rootView.findViewById(R.id.listview_detailactivity1);
+
+//             Log.d(LOGG_TAG, "The postAdapter is: " + postTypeAdapter.toString());
+             listView.setAdapter(postTypeAdapter);
+
+             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                 @Override
+                 public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                     String subReddit = postTypeAdapter.getItem(position);
+                     Log.v(LOGG_TAG, "The URL being passed to the web Browser is " + STRING_URL);
+                     String url = STRING_URL;
+                     Intent i = new Intent(Intent.ACTION_VIEW);
+                     i.setData(Uri.parse(url));
+                     startActivity(i);
+                 }
+             });
+
+             return rootView;
+         }
+         return null;
+    }
+
+    public class FetchTopicTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchTopicTask.class.getSimpleName();
+
+        public FetchTopicTask(Void... params){
+
+        }
+        @Override
+        protected String[] doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //This will contain the raw JSON String response as a String
+            String redditJsonStr = null;
+            String topic = params[0];
+
+            if(!topic.equals("")){
+                data = "r/" + topic + "/" + data;
+            }
+
+            Log.d(LOG_TAG,"The data is " + data);
+
+            try {
+            //construct the url for the entry of the reddit API
+               String tempURL = "http://www.reddit.com/" + data + "/.json";
+               System.out.println(tempURL);
+               URL url = new URL(tempURL);
+
+               // Create the request to Reddit API, and open the connection
+               urlConnection = (HttpURLConnection) url.openConnection();
+               urlConnection.setRequestMethod("GET");
+               urlConnection.connect();
+
+               // Read the input stream into a String
+               InputStream inputStream = urlConnection.getInputStream();
+               StringBuffer buffer = new StringBuffer();
+               if (inputStream == null) {
+               // Nothing to do.
+                  redditJsonStr = null;
+               }
+               reader = new BufferedReader(new InputStreamReader(inputStream));
+
+               String line;
+               while ((line = reader.readLine()) != null) {
+               // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+               // But it does make debugging a *lot* easier if you print out the completed
+               // buffer for debugging.
+                  buffer.append(line + "\n");
+               }
+
+               if (buffer.length() == 0) {
+               // Stream was empty.  No point in parsing.
+                  redditJsonStr = null;
+               }
+
+                redditJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Topic JSON String " + redditJsonStr);
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                redditJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                   try {
+                       reader.close();
+                   } catch (final IOException e) {
+                       Log.e("PlaceholderFragment", "Error closing stream", e);
+                   }
+                }
+
+                try {
+                    return getSubRedditDataFromJSON(redditJsonStr);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+
+        private String[] getSubRedditDataFromJSON(String redditJsonStr) throws JSONException{
+
+            //these are the names of the JSON objects that need to be extracted
+            final String R_AUTHOR = "author";
+            final String R_TITLE = "title";
+            final String R_PERMALINK = "permalink"; //the links to the comments on that article
+            final String R_URL = "url";
+            final String R_SUBREDDIT = "subreddit";
+            final String R_DATA = "data";
+            final String R_CHILDREN = "children";
+
+            String[] resultStr = new String[10];
+
+            JSONObject redditJson = new JSONObject(redditJsonStr);
+            JSONObject dataObject = redditJson.getJSONObject(R_DATA);
+            JSONArray children = dataObject.getJSONArray(R_CHILDREN);
+
+            for (int i = 0; i < 10 ; i++) {
+                JSONObject dummy = children.getJSONObject(i);
+                JSONObject data = dummy.getJSONObject(R_DATA);
+                String author = data.getString(R_AUTHOR);
+                String title = data.getString(R_TITLE);
+                String permalink = data.getString(R_PERMALINK);
+                STRING_URL = data.getString(R_URL);
+                String subreddit = data.getString(R_SUBREDDIT);
+
+                resultStr[i] = "Title: " + title + " by " + author;
+
+            }
+
+//            for (String s : resultStr){
+//                Log.v(LOG_TAG, "Topic Entry " + s);
+//            }
+
+            return resultStr;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if(result != null){
+                postTypeAdapter.clear();
+                for(String post : result){
+//                    Log.d(LOG_TAG, " this post is now being added: " + post);
+                    postTypeAdapter.add(post);
+                }
+            }
+
+        }
+    }
 }
+
 
