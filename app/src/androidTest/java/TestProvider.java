9eb67263d9539cf51b724928fe5031ecc7b65324
roocheerah <roocheerah@users.noreply.github.com>
2014-09-15 17:56:10 -0700

    Update TestProvider.java

--- app/src/androidTest/java/TestProvider.java
+++ app/src/androidTest/java/TestProvider.java
@@ -1,16 +1,13 @@
 package com.ruchirakulkarni.redditreader;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.test.AndroidTestCase;
-import android.util.Log;
 
 import com.ruchirakulkarni.redditreader.data.RedditContract;
 import com.ruchirakulkarni.redditreader.data.RedditDbHelper;
-
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by ruchirakulkarni on 9/15/14.
@@ -22,80 +19,33 @@ public class TestProvider extends AndroidTestCase {
         mContext.deleteDatabase(RedditDbHelper.DATABASE_NAME);
     }
 
-    static ContentValues getContentValues(){
-        ContentValues values = new ContentValues();
-        int testScore = 200;
-        String testAuthor = "Ruchira";
-        String testSubRedditType = "funny";
-        String testTitle = "Cool person right here";
-        String testPermalink = "r/funny/cool";
-        int testComments = 1000;
-        String testUrl = "http://reddit.com";
-        values.put(RedditContract.RedditPostEntry.COL_AUTHOR, testAuthor);
-        values.put(RedditContract.RedditPostEntry.COL_SCORE, testScore);
-        values.put(RedditContract.RedditPostEntry.COL_SUBREDDIT_TYPE, testSubRedditType);
-        values.put(RedditContract.RedditPostEntry.COL_TITLE, testTitle);
-        values.put(RedditContract.RedditPostEntry.COL_PERMALINK, testPermalink);
-        values.put(RedditContract.RedditPostEntry.COL_COMMENTS, testComments);
-        values.put(RedditContract.RedditPostEntry.COL_URL, testUrl);
+    public void testInsertReadProvider(){
 
-        return values;
-    }
+        ContentValues testValues = TestDb.getContentValues();
+        Uri redditUri = mContext.getContentResolver().insert(RedditContract.RedditPostEntry.CONTENT_URI, testValues);
 
-    public void testInsertReadDb(){
-        RedditDbHelper dbHelper = new RedditDbHelper(mContext);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        long redditRowId = db.insert(RedditContract.RedditPostEntry.TABLE_NAME, null, getContentValues());
+
+        long redditRowId = ContentUris.parseId(redditUri);
+       // long redditRowId = db.insert(RedditContract.RedditPostEntry.TABLE_NAME, null, testValues);
 
         // Verify we got a row back.
         assertTrue(redditRowId != -1);
-        Log.d(LOG_TAG, "New row id: " + redditRowId);
+        //Log.d(LOG_TAG, "New row id: " + redditRowId);
 
         // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
         // the round trip.
 
-        // Specify which columns you want.
-        String[] columns = {
-                RedditContract.RedditPostEntry._ID,
-                RedditContract.RedditPostEntry.COL_AUTHOR,
-                RedditContract.RedditPostEntry.COL_TITLE,
-                RedditContract.RedditPostEntry.COL_COMMENTS,
-                RedditContract.RedditPostEntry.COL_SCORE,
-                RedditContract.RedditPostEntry.COL_COMMENTS,
-                RedditContract.RedditPostEntry.COL_URL,
-                RedditContract.RedditPostEntry.COL_PERMALINK,
-                RedditContract.RedditPostEntry.COL_SUBREDDIT_TYPE
-        };
-
         // A cursor is your primary interface to the query results.
-        Cursor cursor = db.query(
-                RedditContract.RedditPostEntry.TABLE_NAME,  // Table to Query
-                columns,
+        Cursor cursor = mContext.getContentResolver().query(
+                RedditContract.RedditPostEntry.CONTENT_URI,
                 null, // Columns for the "where" clause
                 null, // Values for the "where" clause
                 null, // columns to group by
-                null, // columns to filter by row groups
                 null // sort order
         );
 
-        validateCursor(cursor, getContentValues());
-        db.close();
+        TestDb.validateCursor(cursor, testValues);
 
-    }
-
-    static void validateCursor(Cursor valueCursor, ContentValues expectedValues) {
-
-        assertTrue(valueCursor.moveToFirst());
-
-        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
-        for (Map.Entry<String, Object> entry : valueSet) {
-            String columnName = entry.getKey();
-            int idx = valueCursor.getColumnIndex(columnName);
-            assertFalse(idx == -1);
-            String expectedValue = entry.getValue().toString();
-            assertEquals(expectedValue, valueCursor.getString(idx));
-        }
-        valueCursor.close();
     }
 
     public void testGetType() {
@@ -103,8 +53,5 @@ public class TestProvider extends AndroidTestCase {
         String type = mContext.getContentResolver().getType(RedditContract.RedditPostEntry.CONTENT_URI);
         // vnd.android.cursor.dir/com.ruchirakulkarni.redditreader/reddit/
         assertEquals(RedditContract.RedditPostEntry.CONTENT_TYPE, type);
-
-        
-
     }
 }
