81eee63e3ae305eca5f3744283eb5fd0db332525
roocheerah <roocheerah@users.noreply.github.com>
2014-09-19 11:25:27 -0700

    Update RedditReaderSyncAdapter.java

--- app/src/main/java/com/ruchirakulkarni/redditreader/sync/RedditReaderSyncAdapter.java
+++ app/src/main/java/com/ruchirakulkarni/redditreader/sync/RedditReaderSyncAdapter.java
@@ -1,23 +1,114 @@
 package com.ruchirakulkarni.redditreader.sync;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.SyncRequest;
 import android.content.SyncResult;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ruchirakulkarni.redditreader.R;
 
 /**
  * Created by ruchirakulkarni on 9/18/14.
  */
 public class RedditReaderSyncAdapter extends AbstractThreadedSyncAdapter {
 
+    public final String LOG_TAG = RedditReaderSyncAdapter.class.getSimpleName();
+
+    // Interval at which to sync with the weather, in milliseconds.
+    // 1000 milliseconds (1 second) * 60 seconds (1 minute) * 180 = 3 hours
+    public static final int SYNC_INTERVAL = 60 * 180;
+    public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
+
+    private final Context mContext;
+
     public RedditReaderSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
+        Log.d(LOG_TAG, "Creating SyncAdapter");
+        mContext = context;
     }
 
     @Override
-    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
+    public void onPerformSync(Account account, Bundle bundle, String authority,
+                              ContentProviderClient provider, SyncResult syncResult) {
+        Log.d(LOG_TAG, "Starting sync");
+        // Getting the zipcode to send to the API
+        //String locationQuery = Utility.getPreferredLocation(mContext);
+    }
 
+
+    public static Account getSyncAccount(Context context) {
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+
+        // Create the account type and default account
+        Account newAccount = new Account(
+                context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
+
+        // If the password doesn't exist, the account doesn't exist
+        if (null == accountManager.getPassword(newAccount)) {
+            // Add the account and account type, no password or user data
+            // If successful, return the Account object, otherwise report an error.
+            if (!accountManager.addAccountExplicitly(newAccount, "", null)) {
+                return null;
+            }
+
+            // If you don't set android:syncable="true" in
+            // in your <provider> element in the manifest,
+            // then call context.setIsSyncable(account, AUTHORITY, 1)
+            // here.
+            onAccountCreated(newAccount, context);
+        }
+        return newAccount;
+    }
+
+    public static void syncImmediately(Context context) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        ContentResolver.requestSync(getSyncAccount(context),
+                context.getString(R.string.content_authority), bundle);
+    }
+
+    /**
+     * Helper method to schedule the sync adapter periodic execution
+     */
+    public static void configurePeriodicSync(Context context, int syncInterval, int flexTime) {
+
+        Account account = getSyncAccount(context);
+        String authority = context.getString(R.string.content_authority);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // we can enable inexact timers in our periodic sync
+            SyncRequest request = new SyncRequest.Builder().
+                    syncPeriodic(syncInterval, flexTime).
+                    setSyncAdapter(account, authority).build();
+            ContentResolver.requestSync(request);
+        } else {
+            ContentResolver.addPeriodicSync(account,
+                    authority, new Bundle(), syncInterval);
+        }
+    }
+
+    private static void onAccountCreated(Account newAccount, Context context) {
+
+        // Schedule the sync for periodic execution
+        RedditReaderSyncAdapter.configurePeriodicSync(context, SYNC_INTERVAL, SYNC_FLEXTIME);
+
+        // Without calling setSyncAutomatically, our periodic sync will not be enabled.
+        ContentResolver.setSyncAutomatically(newAccount, context.getString(R.string.content_authority), true);
+
+        // Let's do a sync to get things started.
+        syncImmediately(context);
+    }
+
+    public static void initializeSyncAdapter(Context context) {
+        getSyncAccount(context);
     }
 }
